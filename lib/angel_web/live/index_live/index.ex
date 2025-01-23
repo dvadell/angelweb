f7a714789d462f5adb21008b121e61a4c811d504
Diego M. Vadell <diego.m.vadell@gmail.com>
2025-01-23 19:21:32 +0000

    Raw list of graphs in /graphs/

--- lib/angel_web/live/index_live/index.ex
+++ lib/angel_web/live/index_live/index.ex
@@ -3,20 +3,31 @@ defmodule AngelWeb.IndexLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :graph_data, fetch_graph_data())}
+    {:ok, assign(socket, :graphs, list_graphs())}
   end
 
-  defp fetch_graph_data() do
-    url = "http://localhost:80/render?target=stats.gauges.jr.load_avg&from=-24hours&format=json"
+  defp list_graphs() do
+    url = "http://localhost:80/metrics/find?query=stats.gauges.jr.*"
     response = HTTPoison.get!(url)
     Jason.decode!(response.body)
+    |> IO.inspect(label: :original)
+    |> Enum.filter( fn graph -> Map.get(graph, "leaf") == 1 end)
+    |> IO.inspect
   end
 
   @impl true
   def render(assigns) do
     ~H"""
-    <h1>Graphs</h1>
-    <canvas id="myChart"></canvas>
+    <h1>Graphs List</h1>
+    <ul>
+    <%= Enum.map(@graphs, fn g -> %>
+    <li>
+      <a href={"/graphs/#{Map.get(g, "id")}"}>
+        <%= Map.get(g, "id") %>
+      </a>
+    </li>
+    <% end) %>
+    </ul>
     """
   end
 end
