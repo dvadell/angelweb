e7badb843949c505950dda72bf76acda41f9cbbf
diego.m.vadell@gmail.com <Diego M. Vadell>
2025-08-12 21:16:37 +0000

    Config Angel.NonTransactionalRepo

--- config/dev.exs
+++ config/dev.exs
@@ -7,6 +7,10 @@ config :angel, Angel.Repo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
+config :angel, Angel.NonTransactionalRepo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 10
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
