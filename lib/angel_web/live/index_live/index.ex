823967fe74b57c654696deec9a13b17953ad3b18
Diego M. Vadell <diego.m.vadell@gmail.com>
2025-09-12 18:44:51 +0000

    Add a filter to filter graphs in /graphs

--- lib/angel_web/live/index_live/index.ex
+++ lib/angel_web/live/index_live/index.ex
@@ -4,10 +4,40 @@ defmodule AngelWeb.IndexLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :graphs, list_graphs())}
+    graphs = list_graphs()
+
+    {:ok,
+     socket
+     |> assign(:graphs, graphs)
+     |> assign(:filtered_graphs, graphs)
+     |> assign(:filter, nil)}
+  end
+
+  @impl true
+  def handle_event("filter", %{"q" => q}, socket) do
+    graphs = socket.assigns.graphs
+    filtered_graphs = filter_graphs(graphs, q)
+
+    socket =
+      socket
+      |> assign(:filtered_graphs, filtered_graphs)
+      |> assign(:filter, q)
+
+    {:noreply, socket}
   end
 
   defp list_graphs do
     Graphs.list_graphs()
+  end
+
+  defp filter_graphs(graphs, nil) do
+    graphs
+  end
+
+  defp filter_graphs(graphs, filter) do
+    graphs
+    |> Enum.filter(fn g ->
+      String.contains?(g.short_name, filter)
+    end)
   end
 end
